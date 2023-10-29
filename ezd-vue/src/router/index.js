117d8f9c31837a6fa3b20d30dd8a091c74386e86
sally <119037864+ht18boynd@users.noreply.github.com>
2023-10-29 21:13:56 +0700

    v-29-10 v1

--- ezd-vue/src/router/index.js
+++ ezd-vue/src/router/index.js
@@ -4,11 +4,16 @@ import HomePage from '@/components/Home.vue'
 import AboutPage from '@/components/About.vue'
 import BecomeIdol from '@/components/BecomeIdol.vue'
 import GameIndex from '@/components/miniGame/index.vue'
+import testCapcha from '@/components/testCapcha.vue'
 
 const routes = [
     {
         path: '/',
         component: HomePage
+    },
+    {
+        path: '/test',
+        component: testCapcha
     },
     {
         path: '/about',
